59f27470a2fd658cdde424b1cd985283aeeeef25
desipossa <c1jang@hanmail.net>
2025-03-07 12:14:34 +0900

    20250307

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,7 @@ $(function () {
         .from('.main_visual .me', { x: 640, opacity: 0, rotation: 360 });
 
 
+    //큐브 슬라이드
     const hs = new Swiper('.header .cube .header_slide', {
         loop: true,
         effect: "cube",
@@ -21,19 +22,30 @@ $(function () {
     })
 
     $('.wrapper').fullpage({
-        anchors: ['intro', 'portfolio', 'profile'],
+        anchors: ['intro', 'portfolio01', 'portfolio02', 'portfolio03', 'training', 'profile'],
         fixedElements: '.header, .footer',
+        responsiveWidth: 1200,
         onLeave: function (o, d, dr) {
             let idx = d.index;
             if (idx == 0) {
                 tl.restart();
             }
 
+            //슬라이드 돌아가게
             hs.slideToLoop(idx);
 
 
             $('.header .side_nav li').removeClass('on');
             $('.header .side_nav li').eq(idx).addClass('on');
+
+
+        },
+
+        afterLoad: function (o, d, dr) {
+            let idx = d.index;
+
+            $('.section').removeClass('on');
+            $('.section').eq(idx).addClass('on');
         }
     });
 
@@ -41,4 +53,18 @@ $(function () {
 
 $(function () {
 
+    $('.header .btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.header .cover_lnk').toggleClass('on');
+    });
+
+    $('.header .cover_lnk a').on('click', function () {
+        $('.header .btn').removeClass('on');
+        $('.header .cover_lnk').removeClass('on');
+    });
+
+    $('.header .cover_lnk').on('wheel', function (e) {
+        e.preventDefault();
+        return false;
+    })
 })
